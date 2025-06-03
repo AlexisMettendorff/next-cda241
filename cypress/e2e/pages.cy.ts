b8595c814fd4bba1e38c14e24c21d5d6c85f6997
AlexisMettendorff <alexis.mettendorff@viacesi.fr>
2025-06-03 13:40:36 +0200

    Change to localhost 3000

--- cypress/e2e/pages.cy.ts
+++ cypress/e2e/pages.cy.ts
@@ -6,7 +6,7 @@
 describe("Navigation", () => {
   it("should navigate to the about page", () => {
     // Start from the index page
-    cy.visit("http://localhost:3001/home");
+    cy.visit("http://localhost:3000/home");
 
     // Find a link with an href attribute containing "about" and click it
     cy.get('a[href*="about"]').click();
